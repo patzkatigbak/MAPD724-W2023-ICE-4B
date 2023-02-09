83ad88c19073c5186d99a8b0ba309a92073b0243
patzkatigbak <patzkatigbak@gmail.com>
2023-02-08 19:38:42 -0500

    added start scene

--- MAPD724-W2023-ICE-4B/GameViewController.swift
+++ MAPD724-W2023-ICE-4B/GameViewController.swift
@@ -3,50 +3,88 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
+    //instance variables
+    var currentScene: GKScene?
+    
     @IBOutlet weak var LivesLabel: UILabel!
     @IBOutlet weak var ScoreLabel: UILabel!
+    @IBOutlet weak var StartLabel: UILabel!
+    @IBOutlet weak var StartButton: UIButton!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        if let scene = GKScene(fileNamed: "GameScene")
-        {
-            if let sceneNode = scene.rootNode as! GameScene?
-            {
-                sceneNode.scaleMode = .aspectFill
-                if let view = self.view as! SKView?
-                {
-                    view.presentScene(sceneNode)
-                    view.ignoresSiblingOrder = true
-                }
-            }
-        }
+       
+        presentStartScene()
         
-        // Initialize the Lives and Score
-                CollisionManager.gameViewController = self
-                ScoreManager.Score = 0
-                ScoreManager.Lives = 5
-                updateLivesLabel()
-                updateScoreLabel()
-    }
+        CollisionManager.gameViewController = self
 
+    }
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask
     {
         return .portrait
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
     func updateLivesLabel()
-       {
-           LivesLabel.text = "Lives: \(ScoreManager.Lives)"
-       }
-       
-       func updateScoreLabel()
-       {
-           ScoreLabel.text = "Score: \(ScoreManager.Score)"
-       }
+    {
+        LivesLabel.text = "Lives: \(ScoreManager.Lives)"
+    }
+    
+    func updateScoreLabel()
+    {
+        ScoreLabel.text = "Score: \(ScoreManager.Score)"
+    }
+    
+    func setScene(sceneName: String) -> Void
+    {
+        currentScene = GKScene(fileNamed: sceneName)
+        
+        if let scene = currentScene!.rootNode as! SKScene?
+        {
+            scene.scaleMode = .aspectFill
+            if let view = self.view as! SKView?
+            {
+                view.presentScene(scene)
+                view.ignoresSiblingOrder = true
+            }
+        }
+        
+    }
+    
+    func presentStartScene()
+    {
+        ScoreLabel.isHidden = true
+        LivesLabel.isHidden = true
+        StartLabel.isHidden = false
+        StartButton.isHidden = false
+        setScene(sceneName: "StartScene")
+        
+    }
+    
+    func presentEndScene()
+    {
+        
+    }
+    
+    @IBAction func StartButton_Pressed(_ sender: UIButton)
+    {
+        ScoreLabel.isHidden = false
+        LivesLabel.isHidden = false
+        StartLabel.isHidden = true
+        StartButton.isHidden = true
+        
+        // Initialize the Lives and Score
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
+        setScene(sceneName: "GameScene")
+    }
+    
 }
